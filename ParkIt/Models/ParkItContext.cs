dbaefa220e067801579b592218d7e1cc7fd958dd
Max Brockbank <maxbrockbank1999@gmail.com>
2021-01-22 09:47:14 -0800

    add seed data

--- ParkIt/Models/ParkItContext.cs
+++ ParkIt/Models/ParkItContext.cs
@@ -10,5 +10,19 @@ namespace ParkIt.Models
     }
 
     public DbSet<Park> Parks {get;set;}
+
+    protected override void OnModelCreating(Modelbuilder builder)
+    {
+      builder.Entity<Park>()
+        .HasData(
+          new Park {ParkId=1, Name="Crater Lake", State="Oregon", ParkType="National"}
+          new Park {ParkId=2, Name="Grand Canyon", State="Arizona", ParkType="National"}
+          new Park {ParkId=3, Name="Yosemite", State="California", ParkType="National"}
+          new Park {ParkId=4, Name="Redwood", State="California", ParkType="National"}
+          new Park {ParkId=5, Name="Silver Falls", State="Oregon", ParkType="State"}
+          new Park {ParkId=6, Name="Fort Stevens", State="Oregon", ParkType="State"}
+          
+        )
+    }
   }
 }
